183c0ecb1f1d56ec25541e26c5fb13c46f35e8fa
Gleb <tquicklyy@yandex.ru>
2024-09-15 17:29:15 +0300

    Add first logo

--- src/main/java/org/player/videoplayer/HelloApplication.java
+++ src/main/java/org/player/videoplayer/HelloApplication.java
@@ -3,18 +3,20 @@ package org.player.videoplayer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage mainMenuStage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        mainMenuStage.setTitle("Video player");
+        mainMenuStage.getIcons().add(new Image("file:./src/main/resources/images/logo-video-player.png"));
+        mainMenuStage.setScene(scene);
+        mainMenuStage.show();
     }
 
     public static void main(String[] args) {
