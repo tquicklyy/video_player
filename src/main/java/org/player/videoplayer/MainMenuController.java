090270d41f0c4605ae53e4e403a941207131aefd
Gleb <tquicklyy@yandex.ru>
2024-09-24 18:00:25 +0300

    Added scaling of the first window and replaced the background photo of the first window

--- src/main/java/org/player/videoplayer/MainMenuController.java
+++ src/main/java/org/player/videoplayer/MainMenuController.java
@@ -23,7 +23,7 @@ public class MainMenuController {
     @FXML
     void initialize() {
         mainMenuBorderPane.heightProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue.intValue() < 598) {
+            if (newValue.intValue() < 530) {
                 mainMenuFirstLabel.setPrefHeight(54);
                 mainMenuFirstLabel.setPrefWidth(300);
                 mainMenuFirstLabel.setFont(Font.font("Arial", FontWeight.BOLD, 44));
@@ -35,42 +35,66 @@ public class MainMenuController {
                 mainMenuThirdLabel.setPrefHeight(32);
                 mainMenuThirdLabel.setPrefWidth(300);
                 mainMenuThirdLabel.setFont(Font.font("Arial", FontWeight.BOLD, 13));
-            } else if (newValue.intValue() < 758) {
-                mainMenuFirstLabel.setPrefHeight(56);
-                mainMenuFirstLabel.setPrefWidth(325);
-                mainMenuFirstLabel.setFont(Font.font("Arial", FontWeight.BOLD, 45));
+            } else if (newValue.intValue() < 660) {
+                mainMenuFirstLabel.setPrefHeight(67);
+                mainMenuFirstLabel.setPrefWidth(345);
+                mainMenuFirstLabel.setFont(Font.font("Arial", FontWeight.BOLD, 54));
 
                 mainMenuSecondLabel.setPrefHeight(34);
-                mainMenuSecondLabel.setPrefWidth(325);
-                mainMenuSecondLabel.setFont(Font.font("Arial", FontWeight.BOLD, 14));
+                mainMenuSecondLabel.setPrefWidth(345);
+                mainMenuSecondLabel.setFont(Font.font("Arial", FontWeight.BOLD, 15.4));
 
                 mainMenuThirdLabel.setPrefHeight(34);
-                mainMenuThirdLabel.setPrefWidth(325);
-                mainMenuThirdLabel.setFont(Font.font("Arial", FontWeight.BOLD, 14));
-            } else if (newValue.intValue() < 918) {
-                mainMenuFirstLabel.setPrefHeight(58);
-                mainMenuFirstLabel.setPrefWidth(350);
-                mainMenuFirstLabel.setFont(Font.font("Arial", FontWeight.BOLD, 45));
+                mainMenuThirdLabel.setPrefWidth(345);
+                mainMenuThirdLabel.setFont(Font.font("Arial", FontWeight.BOLD, 15.4));
+            } else if (newValue.intValue() < 790) {
+                mainMenuFirstLabel.setPrefHeight(78);
+                mainMenuFirstLabel.setPrefWidth(390);
+                mainMenuFirstLabel.setFont(Font.font("Arial", FontWeight.BOLD, 64));
 
                 mainMenuSecondLabel.setPrefHeight(36);
-                mainMenuSecondLabel.setPrefWidth(350);
-                mainMenuSecondLabel.setFont(Font.font("Arial", FontWeight.BOLD, 15));
+                mainMenuSecondLabel.setPrefWidth(390);
+                mainMenuSecondLabel.setFont(Font.font("Arial", FontWeight.BOLD, 17.8));
 
                 mainMenuThirdLabel.setPrefHeight(36);
-                mainMenuThirdLabel.setPrefWidth(350);
-                mainMenuThirdLabel.setFont(Font.font("Arial", FontWeight.BOLD, 15));
-            }  else {
-                mainMenuFirstLabel.setPrefHeight(60);
-                mainMenuFirstLabel.setPrefWidth(375);
-                mainMenuFirstLabel.setFont(Font.font("Arial", FontWeight.BOLD, 46));
+                mainMenuThirdLabel.setPrefWidth(390);
+                mainMenuThirdLabel.setFont(Font.font("Arial", FontWeight.BOLD, 17.8));
+            }  else if (newValue.intValue() < 920){
+                mainMenuFirstLabel.setPrefHeight(89);
+                mainMenuFirstLabel.setPrefWidth(445);
+                mainMenuFirstLabel.setFont(Font.font("Arial", FontWeight.BOLD, 74));
 
                 mainMenuSecondLabel.setPrefHeight(38);
-                mainMenuSecondLabel.setPrefWidth(375);
-                mainMenuSecondLabel.setFont(Font.font("Arial", FontWeight.BOLD, 16));
+                mainMenuSecondLabel.setPrefWidth(445);
+                mainMenuSecondLabel.setFont(Font.font("Arial", FontWeight.BOLD, 20.2));
 
                 mainMenuThirdLabel.setPrefHeight(38);
-                mainMenuThirdLabel.setPrefWidth(375);
-                mainMenuThirdLabel.setFont(Font.font("Arial", FontWeight.BOLD, 16));
+                mainMenuThirdLabel.setPrefWidth(445);
+                mainMenuThirdLabel.setFont(Font.font("Arial", FontWeight.BOLD, 20.2));
+            } else if (newValue.intValue() < 1050){
+                mainMenuFirstLabel.setPrefHeight(100);
+                mainMenuFirstLabel.setPrefWidth(505);
+                mainMenuFirstLabel.setFont(Font.font("Arial", FontWeight.BOLD, 84));
+
+                mainMenuSecondLabel.setPrefHeight(40);
+                mainMenuSecondLabel.setPrefWidth(505);
+                mainMenuSecondLabel.setFont(Font.font("Arial", FontWeight.BOLD, 22.6));
+
+                mainMenuThirdLabel.setPrefHeight(40);
+                mainMenuThirdLabel.setPrefWidth(505);
+                mainMenuThirdLabel.setFont(Font.font("Arial", FontWeight.BOLD, 22.6));
+            } else {
+                mainMenuFirstLabel.setPrefHeight(111);
+                mainMenuFirstLabel.setPrefWidth(565);
+                mainMenuFirstLabel.setFont(Font.font("Arial", FontWeight.BOLD, 94));
+
+                mainMenuSecondLabel.setPrefHeight(42);
+                mainMenuSecondLabel.setPrefWidth(565);
+                mainMenuSecondLabel.setFont(Font.font("Arial", FontWeight.BOLD, 25));
+
+                mainMenuThirdLabel.setPrefHeight(42);
+                mainMenuThirdLabel.setPrefWidth(565);
+                mainMenuThirdLabel.setFont(Font.font("Arial", FontWeight.BOLD, 25));
             }
         });
     }
